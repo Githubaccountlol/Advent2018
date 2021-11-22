e24570d2fa54e4dd8f0b701a88513ef9beea2e0a
Githubaccountlol <13028836+Githubaccountlol@users.noreply.github.com>
2021-11-22 04:23:46 -0600

    Day 5 part 1 done

--- src/Day5.rs
+++ src/Day5.rs
@@ -1,16 +1,43 @@
-use crate::FileInput;
+use crate::FileInput::{self, GetInput};
 
 pub fn DoPart1()
 {
+    let mut input = Input();
 
+    while React(&mut input){};
+
+    println!("Remaining: {}", input.len());
 }
 
 pub fn DoPart2()
 {
-
+    
 }
 
-fn Input() -> ()
+fn React(string: &mut Vec<char>) -> bool
 {
+    let len = string.len();
+    let mut flag = false;
+    for i in (0..len){
+        let first = string.get(i);
+        let second = string.get(i+1);
 
+        if let Some(&first) = first{
+            if let Some(&second) = second{
+                if first.eq_ignore_ascii_case(&second) && first != second{
+                    string.splice(i..=i+1, []);
+                    flag = true;
+                }
+            }
+        }
+    }
+        
+    return flag;
+}
+
+fn Input() -> Vec<char>
+{
+    let input = GetInput("Day5").trim().to_owned();
+
+    return input.chars().collect();
 }
